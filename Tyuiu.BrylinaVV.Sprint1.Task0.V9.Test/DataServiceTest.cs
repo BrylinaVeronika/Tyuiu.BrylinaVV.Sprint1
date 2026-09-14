f4140a43e4677a26c4d8183d6af8d56a62b3f919
Tyuiu.BrylinaVV.outlook.com <brylina2008@yandex.ru>
2026-09-14 13:41:20 +0500

    Add project files.

---
+++ Tyuiu.BrylinaVV.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BrylinaVV.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.BrylinaVV.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.BrylinaVV.Sprint1.Task0.V9/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.BrylinaVV.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.BrylinaVV.Sprint1.Task0.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Брылина В. В. | ИИПб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнила: Брылина Вероника Вячеславовна | ИИПб-26-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (9/3)*4/3/2+1           *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (9 / 3) * 4 / 3 / 2 + 1                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
